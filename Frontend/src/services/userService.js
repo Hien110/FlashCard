e2299821b5a26966eea8f0900a0a3e1892b4cf6a
Minh Hiển <minhhien30201@gmail.com>
2025-06-30 04:20:05 +0700

    update

--- Frontend/src/services/userService.js
+++ Frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/user";
+const API_URL = "https://flashcard-be-al1a.onrender.com/api/user";
 
 export const registerUser = async (username, password) => {
   try {
